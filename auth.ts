3f7b58e5e1531b9db133a32f52bea28a27f052ab
mukulpadwal <mukulpadwal.me@gmail.com>
2025-01-07 15:47:48 +0530

    Set JWT session max age to 30 days

--- auth.ts
+++ auth.ts
@@ -112,6 +112,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
   },
   secret: process.env.AUTH_SECRET,
 });

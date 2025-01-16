47f4efccf9ec56d72a14193e4be79d2f843b12c5
mukulpadwal <mukulpadwal.me@gmail.com>
2025-01-16 22:30:34 +0530

    Add dashboard API route; implement order aggregation and user authorization checks

---
+++ app/api/dashboard/route.ts
@@ -0,0 +1,114 @@
+import { auth } from "@/auth";
+import ApiResponse from "@/lib/api-response";
+import { connectToDB } from "@/lib/db";
+import { Order } from "@/models";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+export async function GET() {
+  try {
+    const session = await auth();
+
+    if (!session || session.user.role !== "admin") {
+      return NextResponse.json(
+        new ApiResponse("Unauthorized Request...", 400),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connectToDB();
+
+    const orderDetails = await Order.aggregate([
+      {
+        $lookup: {
+          from: "products",
+          localField: "product",
+          foreignField: "_id",
+          as: "productDetails",
+          pipeline: [
+            {
+              $project: {
+                name: 1,
+                description: 1,
+                owner: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "placedBy",
+          foreignField: "_id",
+          as: "buyerDetails",
+          pipeline: [
+            {
+              $project: {
+                email: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          productDetails: {
+            $arrayElemAt: ["$productDetails", 0],
+          },
+          buyerDetails: {
+            $arrayElemAt: ["$buyerDetails", 0],
+          },
+        },
+      },
+      {
+        $match: {
+          "productDetails.owner": mongoose.Types.ObjectId.createFromHexString(
+            session.user.id.toString()
+          ),
+        },
+      },
+      {
+        $group: {
+          _id: "$status",
+          orders: {
+            $push: "$$ROOT",
+          },
+        },
+      },
+      {
+        $addFields: {
+          totalAmount: {
+            $sum: {
+              $map: {
+                input: "$orders",
+                as: "order",
+                in: "$$order.amount",
+              },
+            },
+          },
+        },
+      },
+    ]);
+
+    return NextResponse.json(
+      new ApiResponse("Data fetched successfully...", 200, orderDetails),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("Something went wrong while fetching dashboard data ", error);
+    return NextResponse.json(
+      new ApiResponse(
+        "Internal server error while fetching dashboard data...",
+        500
+      ),
+      {
+        status: 500,
+      }
+    );
+  }
+}

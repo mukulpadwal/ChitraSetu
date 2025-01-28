8a7b48bb67cae87f233e0b012097431a14573ee4
mukulpadwal <mukulpadwal.me@gmail.com>
2025-01-28 16:40:29 +0530

    Enhance routing logic by redirecting authenticated users to /products and unauthenticated users to /login; refactor middleware for clarity

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { Aperture } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 function Logo({ className = "", size }: { className?: string; size?: number }) {
+  const { data: session } = useSession();
+
   return (
     <Link
-      href={"/"}
+      href={`${session ? "/products" : "/"}`}
       className={`${className} flex flex-col sm:flex-row justify-center items-center gap-0 sm:gap-2`}
     >
       <Aperture size={size || 30} />
--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,46 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
+const protectedRoutes = ["/products", "/profile", "/orders", "/admin"];
+const publicRoutes = ["/", "/login", "/signup"];
+const adminRoutes = [
+  "/admin/list-product",
+  "/admin/listed-products",
+  "/admin/dashboard",
+];
+
+const isRouteProtected = (pathname: string) => {
+  const isProductPage = pathname.startsWith("/products/");
+  const isAdminPage = pathname.startsWith("/admin/");
+  const isProfilePage = pathname.startsWith("/profile/");
+  return (
+    protectedRoutes.includes(pathname) ||
+    isProductPage ||
+    isAdminPage ||
+    isProfilePage
+  );
+};
+
+const isAdminRoute = (pathname: string) => adminRoutes.includes(pathname);
+
 export default auth((req) => {
   const { pathname } = req.nextUrl;
   const session = req.auth;
 
-  const protectedRoutes = ["/products", "/profile", "/orders", "/admin"];
-  const publicRoutes = ["/", "/login", "/signup"];
-  const adminRoutes = [
-    "/admin/list-product",
-    "/admin/listed-products",
-    "/admin/dashboard",
-  ];
-
-  const isProductPage = pathname.startsWith("/products/");
-  const isAdminPage = pathname.startsWith("/admin/");
-  const isProfilePage = pathname.startsWith("/profile/");
-
   // Redirect authenticated users away from public routes
   if (session && publicRoutes.includes(pathname)) {
-    const newUrl = new URL("/products", req.nextUrl.origin); // Redirect to /products if already logged in
-    return NextResponse.redirect(newUrl);
+    return NextResponse.redirect(new URL("/products", req.nextUrl.origin));
   }
 
-  // Redirect unauthenticated users away from protected routes, including product pages
-  if (
-    !session &&
-    (protectedRoutes.includes(pathname) ||
-      isProductPage ||
-      isAdminPage ||
-      isProfilePage)
-  ) {
-    const newUrl = new URL("/login", req.nextUrl.origin); // Redirect to /login if not logged in
-    return NextResponse.redirect(newUrl);
+  // Redirect unauthenticated users away from protected routes
+  if (!session && isRouteProtected(pathname)) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl.origin));
   }
 
+  // Restrict non-admin users from accessing admin routes
   if (session && pathname.includes("/admin")) {
-    if (session.user.role !== "admin" && adminRoutes.includes(pathname)) {
-      // We need to stop normal user from accesing the admin routes
-      const newUrl = new URL("/admin", req.nextUrl.origin);
-      return NextResponse.redirect(newUrl);
+    if (session.user.role !== "admin" && isAdminRoute(pathname)) {
+      return NextResponse.redirect(new URL("/admin", req.nextUrl.origin));
     }
   }
 
